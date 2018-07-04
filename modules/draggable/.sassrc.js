f2766cfe7870411b85b7345f0da1c9193fc699e6
kaptinlin <amazingsurge@gmail.com>
2018-07-05 02:25:03 +0800

    refactor(all): add babelrc config to samples

--- modules/draggable/.sassrc.js
+++ modules/draggable/.sassrc.js
@@ -2,7 +2,7 @@ const pkg = require('./package.json')
 
 module.exports = {
   input: pkg.css.source,
-  includePaths: ['../../node_modules', 'node_modules'],
+  includePaths: ['../../../node_modules', 'node_modules'],
   output: [
     { outputStyle: 'nested', file: pkg.css.min },
     { outputStyle: 'compressed', file: pkg.css.main }
