405d5c22e08efc1805066285d06c4e7781aea573
kaptinlin <kaptinlin@gmail.com>
2018-07-04 23:19:49 +0800

    refactor(all): update babel config

--- modules/checkbox/samples/src/index.js
+++ modules/checkbox/samples/src/index.js
@@ -1,9 +1,9 @@
-// import icons
-import '@pluginjs/icons/dist/plugin-icons.css'
-// import checkbox css
-import 'font-awesome/css/font-awesome.css'
-import '@pluginjs/checkbox/src/css/checkbox.scss'
-// import 'css/font-awesome.min.css'
+// // import icons
+// import '@pluginjs/icons/dist/plugin-icons.css'
+// // import checkbox css
+// import 'font-awesome/css/font-awesome.css'
+// import '@pluginjs/checkbox/src/css/checkbox.scss'
+// // import 'css/font-awesome.min.css'
 // import sections
 import './sections/js/inline'
 import './sections/js/icon'
