7421e8ac3732c765c5fe9ccd917fd30b30b9b80e
huanranc <huanranjoe@163.com>
2018-07-12 18:04:55 +0800

    test(countdown): correcting existing tests

--- modules/countdown/__tests__/fixtures/sample.js
+++ modules/countdown/__tests__/fixtures/sample.js
@@ -1,6 +1,6 @@
 import { parseHTML } from '@pluginjs/dom'
 
 export default () => parseHTML`
-<div class="countdown" data-mode="ordinary" data-due="2018-11-22 8:10:50" data-format="Y,M,DM,h,m,s" data-site="above" data-label="年，月，天，时，分，秒">
+<div class="countdown">
 </div>
 `
