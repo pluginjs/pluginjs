7ec3564959e23fcebd538d501262d082ea4abe54
chengfu <chengfu@amazingsurge.com>
2018-07-18 00:48:39 -0700

    fix(date-picker): update samples

--- modules/date-picker/samples/src/index.js
+++ modules/date-picker/samples/src/index.js
@@ -1,4 +1,6 @@
 import '@pluginjs/icons/dist/plugin-icons.css'
+
+import '@pluginjs/date-picker/src/css/date-picker.scss'
 // import sections
 import './sections/default/index.js'
 import './sections/initialized/index.js'
