a01653acc828336311d1bf522896f04aa3871392
chengfu <chengfu@amazingsurge.com>
2018-07-18 19:10:06 -0700

    fix(spinner): fix plugin display problems

--- modules/spinner/samples/src/sections/mousewheel/index.js
+++ modules/spinner/samples/src/sections/mousewheel/index.js
@@ -2,5 +2,4 @@ import { query } from '@pluginjs/dom'
 import Spinner from '@pluginjs/spinner'
 
 const element = query('#mousewheel .example-mousewheel')
-
 Spinner.of(element, { mousewheel: true })
