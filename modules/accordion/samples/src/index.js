02880641f5e68ec4c2077ed6653562cbff2befce
huanranc <huanranjoe@163.com>
2018-07-20 18:07:03 +0800

    style(accordion): update style

--- modules/accordion/samples/src/index.js
+++ modules/accordion/samples/src/index.js
@@ -1,5 +1,6 @@
 // import icons
 import '@pluginjs/icons/dist/plugin-icons.css'
+// import 'font-awesome/css/font-awesome.css'
 // import sections
 import './sections/vertical/index.js'
 import './sections/multiple/index.js'
