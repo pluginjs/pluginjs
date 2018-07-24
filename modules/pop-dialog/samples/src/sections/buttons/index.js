c8c433e443efc80ccad8aa90d01afc00831c27f2
Jeff <jeff@amazingsurge.com>
2018-07-24 10:30:37 +0800

    style(tabs): update tabs style

--- modules/pop-dialog/samples/src/sections/buttons/index.js
+++ modules/pop-dialog/samples/src/sections/buttons/index.js
@@ -4,4 +4,14 @@ import popDialog from '@pluginjs/pop-dialog'
 const element = query('#buttons [data-toggle="default-button"]')
 popDialog.of(element, {
   /** options **/
+  buttons: {
+    ok: {
+      label: 'OK!',
+      color: 'primary',
+      fn: resolve => {
+        console.info('clicked OK!')
+        resolve()
+      }
+    }
+  }
 })
--- modules/tabs/samples/src/sections/topleft/index.js
+++ modules/tabs/samples/src/sections/topleft/index.js
@@ -2,4 +2,7 @@ import { query } from '@pluginjs/dom'
 import Tabs from '@pluginjs/tabs'
 
 const element = query('#topleft .tabs')
-Tabs.of(element, {})
+Tabs.of(element, {
+  navLabelTpl:
+    '<a href="javascript:void(0)"><i class=\'tab-icon icon-char icon-chevron-down\'></i>tab1</a>'
+})
