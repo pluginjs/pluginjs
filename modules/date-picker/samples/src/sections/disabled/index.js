9765b961acb374fea1070ef3b87064817f7998c1
zhaohexiangmemeda <630266363@qq.com>
2018-08-03 20:42:13 -0700

    perf(date-picker): optimize the style of the component

--- modules/date-picker/samples/src/sections/disabled/index.js
+++ modules/date-picker/samples/src/sections/disabled/index.js
@@ -1,9 +1,5 @@
 import { query } from '@pluginjs/dom'
 import DatePicker from '@pluginjs/date-picker'
 
-const element = query('#disabled #calendar-mobile-multiple')
-DatePicker.of(element, {
-  mode: 'multiple',
-  mobileMode: true,
-  disabled: true
-})
+const element = query('#disabled #calendar-multiple')
+DatePicker.of(element, { mode: 'multiple', calendars: '4', disabled: true })
