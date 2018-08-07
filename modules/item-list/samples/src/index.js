0a0b08fd8cd36884a82f8918d9af42ff2361fc6b
chengfu <chengfu@amazingsurge.com>
2018-08-06 18:43:59 -0700

    style(list): update wordpress styles

--- modules/item-list/samples/src/index.js
+++ modules/item-list/samples/src/index.js
@@ -2,7 +2,6 @@
 import '@pluginjs/icons/dist/plugin-icons.css'
 import '@pluginjs/styles/inputs.scss'
 import '@pluginjs/styles/buttons.scss'
-
 // import sections
 import './sections/default/index.js'
 import './sections/simple/index.js'
