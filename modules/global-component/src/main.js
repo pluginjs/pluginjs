3c824f650b3b6b3f5eab11a549974c50ce77f51b
Sepctre <sepctre@amazingsurge.com>
2018-08-28 11:33:17 +0800

    refactor(global-component): remove namsepace

--- modules/global-component/src/main.js
+++ modules/global-component/src/main.js
@@ -1,7 +1,7 @@
 import Component from '@pluginjs/component'
 
 export default class GlobalComponent extends Component {
-  constructor(namespace) {
-    super(namespace, window.document.documentElement)
+  constructor() {
+    super(window.document.documentElement)
   }
 }
