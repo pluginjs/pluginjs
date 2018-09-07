127df5b564ac0139fa75e468d2d88c2ab9f1a309
Jeff <jeff@amazingsurge.com>
2018-09-07 16:08:55 +0800

    style(rate): update style

--- modules/rate/samples/src/sections/otherCharecter/index.js
+++ modules/rate/samples/src/sections/otherCharecter/index.js
@@ -4,6 +4,6 @@ import Rate from '@pluginjs/rate'
 const element = query('#otherCharecter .iconClass')
 Rate.of(element, {
   value: 5,
-  iconClass: 'pj-icon pj-icon-smile',
+  iconClass: 'pj-icon pj-icon-smile-solid',
   iconSize: '26px'
 })
