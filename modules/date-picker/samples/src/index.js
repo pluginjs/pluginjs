aad5e8e8eb41e3a96d3a75a39c7e34cd943aa4d1
Jeff <jeff@amazingsurge.com>
2018-09-21 18:03:50 +0800

    style(map-picker): update style

--- modules/date-picker/samples/src/index.js
+++ modules/date-picker/samples/src/index.js
@@ -1,6 +1,4 @@
 import '@pluginjs/icons/dist/plugin-icons.css'
-import '@pluginjs/date-picker/src/css/date-picker.scss'
-import '@pluginjs/styles/inputs.scss'
 // import sections
 import './sections/default/index.js'
 import './sections/initialized/index.js'
