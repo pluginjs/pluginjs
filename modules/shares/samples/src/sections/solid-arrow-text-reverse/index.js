90fbaa29a7a2c983f280c3bf8cbfdb63fbea6609
Sepctre <sepctre@amazingsurge.com>
2019-10-17 10:09:54 +0800

    refactor(shares): rename arrow to triangle

--- modules/shares/samples/src/sections/solid-arrow-text-reverse/index.js
+++
@@ -1,9 +0,0 @@
-import { queryAll } from '@pluginjs/dom'
-import Shares from '@pluginjs/shares'
-
-const elements = queryAll('#solid-arrow-text-reverse .pj-shares')
-elements.forEach(element => {
-  Shares.of(element, {
-    /** options **/
-  })
-})
--- modules/shares/samples/src/sections/solid-triangle-text/index.js
+++ modules/shares/samples/src/sections/solid-triangle-text/index.js
@@ -1,7 +1,7 @@
 import { queryAll } from '@pluginjs/dom'
 import Shares from '@pluginjs/shares'
 
-const elements = queryAll('#solid-arrow-text .pj-shares')
+const elements = queryAll('#solid-triangle-text .pj-shares')
 elements.forEach(element => {
   Shares.of(element, {
     /** options **/
