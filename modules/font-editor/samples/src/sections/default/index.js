7147e7eb7dea4c11dda388cca10964b6b93bfefa
Sepctre <sepctre@amazingsurge.com>
2020-02-07 10:44:45 +0800

    perf(font-editor): clear console

--- modules/font-editor/samples/src/sections/default/index.js
+++ modules/font-editor/samples/src/sections/default/index.js
@@ -2,5 +2,4 @@ import { query } from '@pluginjs/dom'
 import FontEditor from '@pluginjs/font-editor'
 
 const element = query('#default .example-default')
-const instance = FontEditor.of(element)
-console.log('instance', instance)
+FontEditor.of(element)
